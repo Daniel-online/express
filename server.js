4f192835594e826020fd8668fe997985bee8d58c
daniel-online <danielplayeraffonso@gmail.com>
2025-06-23 21:30:38 -0300

    :: server routing, middleware and posting update ::

--- server.js
+++ server.js
@@ -3,17 +3,36 @@ const express = require('express');
 const app = express();
 
 const PORT = 3000;
+const friends = require('./friendList.json');
+
+app.use((req, res, next)=>{
+    const start = Date.now();
+    next();
+    const delta = Date.now() -start;
+    // console.log(` `);
+    console.log(`${req.method} :: ${req.url} - ${delta}ms`);    
+});
+
+app.use(express.json());
+
 app.get('/', (req,res)=>{
     res.send('::Express crude server::')
 })
 
-app.get('/messages', (req, res)=>{
-    res.send('<ul><li>item 1</li></ul>')
+app.get('/friends', (req, res)=>{
+    res.json(friends);
 })
 
-app.post('/messages', (req, res)=>{
-    console.log('Updating messages...');
-    res.send
+app.get('/friends/:friendsId', (req, res)=>{
+    const friendId = Number(req.params.friendsId);
+    let friend = friends[friendId];
+    if (friend){
+        res.status(200).json(friend);
+    } else{
+        res.status(404).json({
+            error: "::Friend Not Found::"
+        });
+    }
 })
 
 app.listen(PORT, ()=>{
